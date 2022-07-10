7be642c80e186ee40fa3c8bc72d82efa78c82f49
Lierni <lierni.ortiz@gmail.com>
2022-07-10 14:06:37 +0200

    cloudinary_security

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "conect-src": ["'self'", "https:"],
+          "connect-src": ["'self'", "https:"],
           "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com/dg8rkeczb"],
           "media-src": ["'self'", "data:", "blob:"],
           upgradeInsecureRequests: null,
