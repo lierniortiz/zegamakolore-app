c1c2956c0cffea3c3f2d19d5e25de01f0e767e61
Lierni <lierni.ortiz@gmail.com>
2022-07-10 14:16:28 +0200

    cloudinary_security

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,4 +1,5 @@
 module.exports = [
+  'strapi::errors',
   {
     name: "strapi::security",
     config: {
@@ -13,4 +14,12 @@ module.exports = [
       },
     },
   },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
