c5f13ee240feb4b0d149c1990fa70635e6610db5
Lierni <lierni.ortiz@gmail.com>
2022-07-10 14:03:17 +0200

    cloudinary_security

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,16 +1,25 @@
 module.exports = [
+  'strapi::errors',
   {
-  name: "strapi::security",
-  config: {
-    contentSecurityPolicy: {
-      useDefaults: true,
-      directives: {
-        "conect-src": ["'self'", "https:"],
-        "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com/dg8rkeczb"],
-        "media-src": ["'self'", "data:", "blob:"],
-        upgradeInsecureRequests: null,
-      }
-    }
-  }
-  }
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "conect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com/dg8rkeczb"],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,19 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "conect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "https://res.cloudinary.com/dg8rkeczb"],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
